737fe54f6501f1f8ca7e4e62abf3e6959800b8bd
Ryan Noronha <ryannoronha@Ryans-MacBook-Pro.local>
2020-11-06 20:06:13 -0300

    added form styling

--- src/components/QuotesForm.js
+++ src/components/QuotesForm.js
@@ -10,7 +10,7 @@ function QuotesForm() {
 
     const updateFormData = event => setFormData({...formData, [event.target.name]: event.target.value});
 
-    const { loanSize, creditScore, propertyType, occupancy } = formData;
+    const { loanSize, creditScore } = formData;
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,49 +18,51 @@ function QuotesForm() {
     }
 
     return (
-        <form onSubmit={e => handleSubmit(e)}>
-            <div>
-                <label htmlFor="loan-size">Loan Size</label>
-                <input
-                    value={loanSize}
-                    onChange={e => updateFormData(e)}
-                    placeholder=""
-                    type="text"
-                    name="loanSize"
-                    id="loan-size"
-                    required
-                />
+        <form className="form" onSubmit={e => handleSubmit(e)}>
+            <div className="form-categories-container">
+                <div className="form-category">
+                    <label htmlFor="loan-size">Loan Size</label>
+                    <input
+                        value={loanSize}
+                        onChange={e => updateFormData(e)}
+                        placeholder=""
+                        type="text"
+                        name="loanSize"
+                        id="loan-size"
+                        required
+                    />
+                </div>
+                <div className="form-category select-container">
+                    <label htmlFor="property-type">Property Type</label>
+                    <select name="property-type" id="property-type" onChange={e => updateFormData(e)}>
+                        <option value="SingleFamily">SingleFamily</option>
+                        <option value="Condo">Condo</option>
+                        <option value="Townhouse">Townhouse</option>
+                        <option value="MultiFamily">MultiFamily</option>
+                    </select>
+                </div>
+                <div className="form-category">
+                    <label htmlFor="credit-score">Credit Score</label>
+                    <input
+                        value={creditScore}
+                        onChange={e => updateFormData(e)}
+                        placeholder=""
+                        type="text"
+                        name="creditScore"
+                        id="credit-score"
+                        required
+                    />
+                </div>
+                <div className="form-category select-container">
+                    <label htmlFor="occupancy">Occupancy</label>
+                    <select name="occupancy" id="occupancy" onChange={e => updateFormData(e)}>
+                        <option value="SingleFamily">Primary</option>
+                        <option value="Condo">Secondary</option>
+                        <option value="Townhouse">Investment</option>
+                    </select>
+                </div>
             </div>
-            <div>
-                <label htmlFor="credit-score">Credit Score</label>
-                <input
-                    value={creditScore}
-                    onChange={e => updateFormData(e)}
-                    placeholder=""
-                    type="text"
-                    name="creditScore"
-                    id="credit-score"
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="property-type">Property Type</label>
-                <select name="property-type" id="property-type" onChange={e => updateFormData(e)}>
-                    <option value="SingleFamily">SingleFamily</option>
-                    <option value="Condo">Condo</option>
-                    <option value="Townhouse">Townhouse</option>
-                    <option value="MultiFamily">MultiFamily</option>
-                </select>
-            </div>
-            <div>
-                <label htmlFor="occupancy">Occupancy</label>
-                <select name="occupancy" id="occupancy" onChange={e => updateFormData(e)}>
-                    <option value="SingleFamily">Primary</option>
-                    <option value="Condo">Secondary</option>
-                    <option value="Townhouse">Investment</option>
-                </select>
-            </div>
-            <button className="submit-btn" type="submit">Quote Rates</button>
+            <button className="button" type="submit">Quote Rates</button>
         </form>
     )
 }
