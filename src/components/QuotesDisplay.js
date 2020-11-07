ff55333b37024fdfaca12a286b2cb1ffb0cdc5c2
Ryan Noronha <ryannoronha@Ryans-MacBook-Pro.local>
2020-11-07 19:10:44 -0300

    implemented display fetched quotes in QuotesDisplay component

--- src/components/QuotesDisplay.js
+++ src/components/QuotesDisplay.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from "react-redux";
 
 function QuotesDisplay() {
+    const quotes = useSelector(state => state.quotes);
+
     return (
         <div>
-            Display Goes Here
+            <table className="table">
+                    <thead>
+                        <tr>
+                        <th>LENDER</th>
+                        <th>PRODUCT</th>
+                        <th>RATE</th>
+                        <th>CLOSING<br/>COSTS</th>
+                        <th>MONTHLY<br/>PAYMENTS</th>
+                        <th>APR<br/></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {quotes.map((quote) => (
+                            <tr key={quote.quote_id}>
+                                <td>{quote.lenderName}</td>
+                                <td>{quote.loanType}</td>
+                                <td>{quote.interestRate}</td>
+                                <td>{quote.closingCosts}</td>
+                                <td>{quote.monthlyPayment}</td>
+                                <td>{quote.apr}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
         </div>
     )
 }
