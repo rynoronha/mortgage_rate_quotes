7a47dcd413e860ed95f08d814229f70fce193c54
Ryan Noronha <ryannoronha@Ryans-MacBook-Pro.local>
2020-11-12 10:43:21 -0300

    implemented unit testing for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup  } from './test-utils'
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+afterEach(cleanup);
+
+test('renders app title', () => {
+  render(<App />);
+
+  expect(screen.getByText(/Mortgage Rate Quotes/)).toBeInTheDocument();
+});
+
+test('it matches snapshot', () => {
+  const tree = render(<App />);
+
+  expect(tree).toMatchSnapshot();
+})
